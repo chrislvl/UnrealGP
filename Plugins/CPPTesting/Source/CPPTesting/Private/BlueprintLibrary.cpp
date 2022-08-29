395911e49ad8dcbb39122a32c6a648858f9a2a91
Chris <heimerl.christian@gmail.com>
2022-08-29 17:19:15 +0200

    Revert "Adds C++ to the project & adds a plugin with a BlueprintFunctionLibrary"
    This reverts commit a7d0e53e80cc875048197b09f5b59d23460a5a2d.

--- Plugins/CPPTesting/Source/CPPTesting/Private/BlueprintLibrary.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "BlueprintLibrary.h"
-
-void UBlueprintLibrary::Print(FString MyText)
-{
-	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, MyText);
-}
